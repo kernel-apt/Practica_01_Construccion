ef49616d3a267fc4f13384896d7cd268e5c9399a
kernel-apt <italobrothers25@gmail.com>
2025-09-28 14:31:35 -0600

    se da formato al module-info.java

--- practica_01/src/main/java/module-info.java
+++ practica_01/src/main/java/module-info.java
@@ -1,7 +1,9 @@
-module controllers {
+module controllers 
+{
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.base;
+    
     opens controllers to javafx.fxml;
     exports controllers;
 }
