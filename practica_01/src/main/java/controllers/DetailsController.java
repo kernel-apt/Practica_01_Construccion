2e67046afa2639e8bf2fd83e698a19fa04a5ffc9
kernel-apt <italobrothers25@gmail.com>
2025-09-29 06:13:59 -0600

    se agrega la vista details.fxml y su respectivo css para presentacion

---
+++ practica_01/src/main/java/controllers/DetailsController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import java.lang.classfile.Label;
+
+import javafx.fxml.FXML;
+
+/**
+ * clase que gestiona los detalles de un producto
+ * cuando desde la tabla se da clic sobre el
+ */
+public class DetailsController 
+{
+     @FXML private Label lbl
+}
